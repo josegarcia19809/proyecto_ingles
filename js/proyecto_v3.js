adf60c32c9eaf831f3e4f1764c8d803cff34797d
Jgarcia <josegarcia19809@gmail.com>
2021-11-10 12:38:56 -0600

    version 3 terminada

--- js/proyecto_v3.js
+++ js/proyecto_v3.js
@@ -2,9 +2,20 @@
 const grupoTarjetas = document.querySelector(".card-group");
 
 function crearTarjeta() {
+    // Asignar valores a los diferentes elementos de la tarjeta
+    const palabra = "Police";
+    const significado = `
+        an official organization whose job is to make people obey the law and
+        to prevent and solve crime; the people who work for this organization
+    `;
+    const ejemplo = "Get out of the house or I'll call the <b>police</b>";
+    const ipa_am = "/pəˈliːs/";
+    const ipa_br = "/pəˈliːs/";
+    const archivoImagen = "police.jpg";
+    
     // Crear la imagen
     const imagen = document.createElement("img");
-    imagen.src = "img/police.jpg";
+    imagen.src = `img/${archivoImagen}`;
     imagen.classList = "card-img-top";
 
     // <div class="card" style="width: 22rem;">
@@ -20,25 +31,24 @@ function crearTarjeta() {
     const cuerpoTarjeta = document.createElement("div");
     cuerpoTarjeta.classList = "card-body";
     tarjeta.appendChild(cuerpoTarjeta);
-
+    
     // Agregar directamente todo lo que lleva el cuerpo de la tarjeta
     cuerpoTarjeta.innerHTML = `
-        <h1>Police</h1>
-        <p class="card-text">an official organization whose job is to make people obey the law and to
-            prevent
-            and solve crime; the people who work for this organization</p>
+        <h1>${palabra}</h1>
+        <p class="card-text">${significado} </p>
 
         <div class="alert alert-success" role="alert">
-            <p><b>Example:</b></p>Get out of the house or I'll call the <b>police</b>.
+            <p><b>Example:</b></p>
+            ${ejemplo}
         </div>
 
         <hr>
 
         <div class="alert alert-primary" role="alert">
-            <b>Británico:</b> /pəˈliːs/
+            <b>Británico:</b> ${ipa_br}
         </div>
         <div class="alert alert-danger" role="alert">
-            <b>Americano:</b> /pəˈliːs/
+            <b>Americano:</b> ${ipa_am}
         </div>
 
         <a href="#" class="btn btn-primary" id="audio_br">Audio BR</a>
